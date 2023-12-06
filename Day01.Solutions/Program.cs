b6fdbbdda79f28e0f50e3d4c1b8e27224c34bd64
marchaen <marchaen.dev@gmail.com>
2023-12-06 10:39:45 +0100

    Copy puzzle input to build dir for more stability

--- Day01.Solutions/Program.cs
+++ Day01.Solutions/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Day01.Solutions;
 
-var input = File.ReadAllText("../input/Day01.txt");
+var input = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "./Day01.txt");
 
 Console.WriteLine("Solution for part one: " + Solution.PartOne(input));
 Console.WriteLine("Solution for part two: " + Solution.PartTwo(input));
-
