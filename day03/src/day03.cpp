11c60adc65378a0e001114118bb523100476d0ef
marchaen <marchaen.dev@gmail.com>
2023-12-07 16:18:43 +0100

    Add wrong solution for day 03 part two
    Unfortunately all the tests pass but the real result is still
    wrong anyway. This is definitely one of the negatives of advent of
    code that when the solution works for the example it doesn't have
    to work for the real input.

--- day03/src/day03.cpp
+++ day03/src/day03.cpp
@@ -1,7 +1,31 @@
 #include "day03.hpp"
 
 #include <cctype>
+#include <optional>
+#include <unordered_map>
 #include <vector>
+
+std::vector<std::string> split_lines(const std::string &input) {
+    std::vector<std::string> lines;
+
+    // Split on new lines
+    int last_line_start = 0;
+
+    for (int index = 0; index < input.length(); index++) {
+        if (input[index] == '\n') {
+            lines.push_back(
+                input.substr(last_line_start, index - last_line_start));
+            last_line_start = index + 1;
+        }
+    }
+
+    // Don't forget the last line
+    if (last_line_start != input.length()) {
+        lines.push_back(input.substr(last_line_start));
+    }
+
+    return lines;
+}
 
 bool is_adjacent_to_symbol(const std::vector<std::string> &lines, int line,
                            int number_start, int number_end) {
@@ -53,24 +77,7 @@ bool is_adjacent_to_symbol(const std::vector<std::string> &lines, int line,
 int solve_part_one(const std::string &input) {
     using namespace std;
 
-    vector<string> lines;
-
-    // Split on new lines
-    int last_line_start = 0;
-
-    for (int index = 0; index < input.length(); index++) {
-        if (input[index] == '\n') {
-            lines.push_back(
-                input.substr(last_line_start, index - last_line_start));
-            last_line_start = index + 1;
-        }
-    }
-
-    // Don't forget the last line
-    if (last_line_start != input.length()) {
-        lines.push_back(input.substr(last_line_start));
-    }
-
+    auto lines = split_lines(input);
     int part_number = 0;
 
     for (int line = 0; line < lines.size(); line++) {
@@ -108,4 +115,130 @@ int solve_part_one(const std::string &input) {
     return part_number;
 }
 
-int solve_part_two(const std::string &input) { return 0; }
+struct Position {
+    int row;
+    int column;
+
+    Position(int row, int column) : row(row), column(column) {}
+
+    bool operator==(const Position &other) const {
+        return this->row == other.row && this->column == other.column;
+    }
+};
+
+struct PositionHash {
+    size_t operator()(const Position &position) const noexcept {
+        size_t result = 0;
+
+        result ^= std::hash<int>{}(position.row) + 0x9e3779b9;
+        result ^= std::hash<int>{}(position.column) + 0x9e3779b9 +
+                  (result << 6) + (result >> 2);
+
+        return result;
+    }
+};
+
+std::optional<Position>
+find_gear_near_number(const std::vector<std::string> &lines, int line,
+                      int number_start, int number_end) {
+    int row_offset = -1;
+    int max_rows = 3;
+
+    if (line == 0) {
+        row_offset = 0;
+        max_rows = 2;
+    }
+
+    if (line == lines.size() - 1) {
+        max_rows = 2;
+    }
+
+    // Note: All input lines need to have the same length. To not cause ub if
+    // this assumption is wrong the first access into the line uses the
+    // at(int index) method which performs bounds checks.
+    int line_length = lines[0].length();
+
+    int column_offset = -1;
+    int max_columns = number_end - number_start + 2;
+
+    if (number_start == 0) {
+        column_offset = 0;
+        max_columns -= 1;
+    }
+
+    for (int row = 0; row < max_rows; row++) {
+        int actual_row = line + row + row_offset;
+
+        for (int column = 0; column < max_columns; column++) {
+            int actual_column = number_start + column + column_offset;
+
+            if (lines[actual_row].at(actual_column) == '*') {
+                return Position(actual_row, actual_column);
+            }
+        }
+    }
+
+    return {};
+}
+
+int solve_part_two(const std::string &input) {
+    using namespace std;
+
+    auto lines = split_lines(input);
+    unordered_map<Position, int, PositionHash> gears;
+    int part_number = 0;
+
+    for (int line = 0; line < lines.size(); line++) {
+        int number_start = -1;
+
+        for (int index = 0; index < lines[line].length(); index++) {
+            if (isdigit(lines[line][index])) {
+                if (number_start == -1) {
+                    number_start = index;
+                    continue;
+                }
+            } else {
+                if (number_start == -1) {
+                    continue;
+                }
+
+                auto maybe_gear =
+                    find_gear_near_number(lines, line, number_start, index);
+
+                if (maybe_gear.has_value()) {
+                    auto gear = maybe_gear.value();
+                    auto number = stoi(
+                        lines[line].substr(number_start, index - number_start));
+
+                    if (gears.count(gear)) {
+                        part_number += gears[gear] * number;
+                    } else {
+                        gears[gear] = number;
+                    }
+                }
+
+                number_start = -1;
+            }
+        }
+
+        // Don't forget the last number
+        int number_end = lines[line].length() - 1;
+
+        if (number_start != -1) {
+            auto maybe_gear =
+                find_gear_near_number(lines, line, number_start, number_end);
+
+            if (maybe_gear.has_value()) {
+                auto gear = maybe_gear.value();
+                auto number =
+                    stoi(lines[line].substr(number_start, number_end));
+
+                if (gears.count(gear)) {
+                    part_number += gears[gear] * number;
+                }
+            }
+        }
+    }
+
+    return part_number;
+}
--- day03/src/tests.cpp
+++ day03/src/tests.cpp
@@ -32,6 +32,33 @@ void test_part_two(const std::string &input) {
     }
 }
 
+void better_test_part_two() {
+    auto input = R"(12.......*..
++.........34
+.......-12..
+..78........
+..*....60...
+78.........9
+.5.....23..$
+8...90$12...
+............
+2.2......12.
+.$.........*
+1.1..503+.56)";
+    auto expected = 6756;
+    auto result = solve_part_two(input);
+
+    std::cout << "Testing part two other: ";
+
+    if (result == expected) {
+        std::cout << "Sucess" << std::endl;
+    } else {
+        std::cout << "Failure" << std::endl;
+        std::cout << "Expected: " << expected << " Got: " << result
+                  << std::endl;
+    }
+}
+
 int main(int argc, char *argv[]) {
     auto input = R"(467..114..
 ...*......
@@ -46,5 +73,6 @@ int main(int argc, char *argv[]) {
 
     test_part_one(input);
     test_part_two(input);
+    better_test_part_two();
     return 0;
 }
