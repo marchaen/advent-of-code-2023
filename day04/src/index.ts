71386b25b3c6b704b1e8883f67b121a727633dda
marchaen <marchaen.dev@gmail.com>
2023-12-13 15:40:20 +0100

    Add correct solution for day 04 part two

--- day04/src/index.ts
+++ day04/src/index.ts
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
 
-import { solvePartOne } from "./lib";
+import { solvePartOne, solvePartTwo } from "./lib";
 
 let input: string = readFileSync("../input/Day04.txt", { encoding: "utf8" });
+
 console.log(`Solution for part one: ${solvePartOne(input)}`);
+console.log(`Solution for part two: ${solvePartTwo(input)}`);
 
