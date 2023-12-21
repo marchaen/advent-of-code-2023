53508a1ee807f44d7919720b9112d53358f38e09
marchaen <marchaen.dev@gmail.com>
2023-12-21 10:06:34 +0100

    Add problem description and tests for day 08

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,4 +1,82 @@
 fn main() {
     let input = include_str!("../../input/Day08.txt");
-    println!("{input}");
+    println!("{}", solution::part_one(input));
+}
+
+mod solution {
+
+    /// Implementation of the solution for the following problem
+    ///
+    /// # Problem description from aoc
+    ///
+    /// One of the camel's pouches is labeled "maps" - sure enough, it's full
+    /// of documents (your puzzle input) about how to navigate the desert. At
+    /// least, you're pretty sure that's what they are; one of the documents
+    /// contains a list of left/right instructions, and the rest of the
+    /// documents seem to describe some kind of **network** of labeled nodes.
+    ///
+    /// It seems like you're meant to use the **left/right** instructions to
+    /// **navigate the network**. Perhaps if you have the camel follow the same
+    /// instructions, you can escape the haunted wasteland!
+    ///
+    /// After examining the maps for a bit, two nodes stick out: `AAA` and
+    /// `ZZZ`. You feel like `AAA` is where you are now, and you have to follow
+    /// the left/right instructions until you reach `ZZZ`.
+    ///
+    /// This format defines each **node** of the network individually. For
+    /// example:
+    ///
+    /// ```
+    /// RL
+    ///
+    /// AAA = (BBB, CCC)
+    /// BBB = (DDD, EEE)
+    /// CCC = (ZZZ, GGG)
+    /// DDD = (DDD, DDD)
+    /// EEE = (EEE, EEE)
+    /// GGG = (GGG, GGG)
+    /// ZZZ = (ZZZ, ZZZ)
+    /// ```
+    ///
+    /// Starting with `AAA`, you need to **look up the next element** based on
+    /// the next left/right instruction in your input. In this example, start
+    /// with `AAA` and go **right** (`R`) by choosing the right element of
+    /// `AAA`, **`CCC`**. Then, `L` means to choose the **left** element of
+    /// `CCC`, **`ZZZ`**. By following the left/right instructions, you reach
+    /// `ZZZ` in **`2`** steps.
+    ///
+    /// Of course, you might not find `ZZZ` right away. If you run out of
+    /// left/right instructions, repeat the whole sequence of instructions as
+    /// necessary: `RL` really means `RLRLRLRLRLRLRLRL...` and so on. For
+    /// example, here is a situation that takes **`6`** steps to reach `ZZZ`:
+    ///
+    /// ```
+    /// LLR
+    ///
+    /// AAA = (BBB, BBB)
+    /// BBB = (AAA, ZZZ)
+    /// ZZZ = (ZZZ, ZZZ)
+    /// ```
+    ///
+    /// Starting at `AAA`, follow the left/right instructions. **How many steps
+    /// are required to reach `ZZZ`?**
+    pub fn part_one(input: &str) -> u32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        const INPUT: &str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(solution::part_one(INPUT), 6)
+    }
 }
