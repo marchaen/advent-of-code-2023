f42086d826694fbd240c05d00294546212b03188
marchaen <marchaen.dev@gmail.com>
2024-01-15 12:32:41 +0100

    Add failing test for day 07 part two

--- Day07.Tests/Tests.cs
+++ Day07.Tests/Tests.cs
@@ -5,18 +5,23 @@ using Day07.Solutions;
 [TestClass]
 public class Tests
 {
+    const string INPUT = """
+        32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483
+    """;
+
     [TestMethod]
     public void PartOne()
     {
-        var input = """
-            32T3K 765
-            T55J5 684
-            KK677 28
-            KTJJT 220
-            QQQJA 483
-        """;
+        Assert.AreEqual(6440, Solution.PartOne(INPUT));
+    }
 
-        Assert.AreEqual(6440, Solution.PartOne(input));
+    [TestMethod]
+    public void PartTwo()
+    {
+        Assert.AreEqual(5905, Solution.PartTwo(INPUT));
     }
 }
-
